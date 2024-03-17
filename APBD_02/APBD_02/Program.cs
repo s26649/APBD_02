656cb318fa24a1b163e515e43f0e7e793765aef5
s26649 <s26649@pjwstk.edu.pl>
2024-03-17 16:59:52 +0100

    Implementacja metody która oblicz średnią

--- APBD_02/APBD_02/Program.cs
+++ APBD_02/APBD_02/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!!!!");
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        Console.WriteLine($"Średnia: {Avg(numbers)}");
+    }
+
+    public static double Avg(int[] numbers)
+    {
+        double sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        return sum / numbers.Length;
+    }
+}
